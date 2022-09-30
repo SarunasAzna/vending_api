ddf91cf342f9ab62d219b4932b4e474a4606c2bd
Sarunas Azna <sarunas.azna@gmail.com>
2022-09-30 11:15:28 +0200

    There is already an active session

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,5 @@
+import json
+
 def test_revoke_access_token(client, seller_headers):
     resp = client.delete("/auth/revoke_access", headers=seller_headers)
     assert resp.status_code == 200
@@ -12,3 +14,13 @@ def test_revoke_refresh_token(client, seller_refresh_headers):
 
     resp = client.post("/auth/refresh", headers=seller_refresh_headers)
     assert resp.status_code == 401
+
+def test_already_an_active_session(seller_user, client):
+    data = {"username": seller_user.username, "password": "seller"}
+    for i in range(2):
+        rep = client.post(
+            "/auth/login",
+            data=json.dumps(data),
+            headers={"content-type": "application/json"},
+        )
+    assert rep.json["message"] == "There is already an active session using your account"
